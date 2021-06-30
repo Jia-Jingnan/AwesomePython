4a86ba0d99fa8ccd955ecfaac9c3cf45cbaa3a9b
Jia-Jingnan <875480307@qq.com>
2021-06-30 10:50:48 +0800

    0630-优化excel读取

--- Alpha/Hotel/LittleFramework_02/excel_util.py
+++ Alpha/Hotel/LittleFramework_02/excel_util.py
@@ -11,6 +11,24 @@ class ExcelUtil():
         self.file = file
         self.sheet_name = sheet_name
 
+
+    # 获取第一行的描述信息，标题行
+    def get_header(self):
+        # 存储标题行的列表
+        workbook = load_workbook(self.file)
+
+        # 定位sheet页
+        sheet = workbook[self.sheet_name]
+        header = []
+        for i in range(1, sheet.max_column + 1):
+            header.append(sheet.cell(1, i).value)
+
+        return header
+
+
+
+
+
     # 调用该方法获得数据后注意数据类型，需要使用eval()函数转换为原来的数据类型
     def get_param(self):
         workbook = load_workbook(self.file)
@@ -26,23 +44,15 @@ class ExcelUtil():
         # 存储所有测试用例数据的列表
         params = []
 
-        for i in range(1, sheet.max_row + 1):
+        for i in range(2, sheet.max_row + 1):
 
             # 定义存储一个测试用例数据的字典
             param = {}
 
-            # res = sheet.cell(i, 1).value
-            # 存入字典
-            param['url'] = sheet.cell(i, 1).value
+            header = self.get_header()
 
-            # 存入字典
-            param['method'] = sheet.cell(i, 2).value
-
-            # 存入字典
-            param['data'] = sheet.cell(i, 3).value
-
-            # 存入字典
-            param['excepted'] = sheet.cell(i, 4).value
+            for j in range(1, sheet.max_column + 1):
+                param[header[j-1]] = sheet.cell(i,j).value
 
             # 将所有的测试用例添加到测试用例列表中
             params.append(param)
