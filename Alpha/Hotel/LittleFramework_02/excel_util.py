262f4d88d5410b284dfc77673e4d5bcba66abbbb
Jia-Jingnan <875480307@qq.com>
2021-07-03 10:39:45 -0800

    0703-给get_param函数添加一个位置参数，该参数为case_id的列表，表示要执行哪些case，默认执行所有case

--- Alpha/Hotel/LittleFramework_02/excel_util.py
+++ Alpha/Hotel/LittleFramework_02/excel_util.py
@@ -11,7 +11,6 @@ class ExcelUtil():
         self.file = file
         self.sheet_name = sheet_name
 
-
     # 获取第一行的描述信息，标题行
     def get_header(self):
         # 存储标题行的列表
@@ -22,15 +21,11 @@ class ExcelUtil():
         header = []
         for i in range(1, sheet.max_column + 1):
             header.append(sheet.cell(1, i).value)
-
         return header
 
-
-
-
-
     # 调用该方法获得数据后注意数据类型，需要使用eval()函数转换为原来的数据类型
-    def get_param(self):
+    def get_param(self, case_ids='all'):
+        cases = []
         workbook = load_workbook(self.file)
 
         # 定位sheet页
@@ -52,11 +47,18 @@ class ExcelUtil():
             header = self.get_header()
 
             for j in range(1, sheet.max_column + 1):
-                param[header[j-1]] = sheet.cell(i,j).value
+                param[header[j-1]] = sheet.cell(i, j).value
 
             # 将所有的测试用例添加到测试用例列表中
             params.append(param)
-        return params
+        if case_ids == 'all':
+            cases = params
+        else:  # 如果range为id的列表[1,2,4]
+            for case in params:
+                if case['id'] in case_ids:
+                    cases.append(case)
+
+        return cases
 
 
 
@@ -68,3 +70,4 @@ if __name__ == '__main__':
     # 调用函数
     params = bravo.get_param()
     print(params)
+
