85c3c76d3cf62b40db0aed28c4578e21aff1e907
Jia-Jingnan <875480307@qq.com>
2021-07-04 10:34:34 -0800

    0704-加入配置文件读取，将配置文件中指定的id放入指定列表中，执行配置文件中特定的测试用例

--- Alpha/Hotel/LittleFramework_02/excel_util.py
+++ Alpha/Hotel/LittleFramework_02/excel_util.py
@@ -3,6 +3,7 @@
 # Desc:
 
 from openpyxl import load_workbook
+from Alpha.Hotel.LittleFramework_02.config_util import ConfigUtil
 
 # 抽象成一个类
 class ExcelUtil():
@@ -24,8 +25,13 @@ class ExcelUtil():
         return header
 
     # 调用该方法获得数据后注意数据类型，需要使用eval()函数转换为原来的数据类型
+    # 用例的可配执行
     def get_param(self, case_ids='all'):
         cases = []
+        case_ids = ConfigUtil().get_config('golf.config', 'CASE_IDS', 'case_ids')
+
+
+        # 配置文件读取要执行id列表
         workbook = load_workbook(self.file)
 
         # 定位sheet页
@@ -51,9 +57,11 @@ class ExcelUtil():
 
             # 将所有的测试用例添加到测试用例列表中
             params.append(param)
+        # 筛选指定要执行的case id
+        print(type(case_ids))
         if case_ids == 'all':
             cases = params
-        else:  # 如果range为id的列表[1,2,4]
+        else:  # 如果case_ids为id的列表[1,2,4]
             for case in params:
                 if case['id'] in case_ids:
                     cases.append(case)
